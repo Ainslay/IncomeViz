c7b62af1ca77a6077a1574999a6bfa6bbc02ecdd
Jakub Spałek <spalek9822@gmail.com>
2020-08-18 15:14:24 +0200

    Laid out basic design for dashboard

--- Frontend/income-viz-web/src/app/app.module.ts
+++ Frontend/income-viz-web/src/app/app.module.ts
@@ -6,22 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PredictionsComponent } from './containers/predictions/predictions.component';
+import { PredictionsListComponent } from './components/predictions-list/predictions-list.component';
+import { PredictionListItemComponent } from './components/prediction-list-item/prediction-list-item.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    PredictionsComponent
+    PredictionsComponent,
+    PredictionsListComponent,
+    PredictionListItemComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/income-viz-web/src/app/components/prediction-list-item/prediction-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prediction-list-item',
+  templateUrl: './prediction-list-item.component.html',
+  styleUrls: ['./prediction-list-item.component.scss']
+})
+export class PredictionListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
