48a9f66ba24ea8954f79e8f464065f18bba3b4ba
Jakub Spałek <spalek9822@gmail.com>
2020-09-02 17:14:44 +0200

    Database now is configured to store currency as string

---
+++ Backend/Module/ProfitCalculation/IncomeViz.ProfitCalculation/Migrations/20200902151005_currency-conversion.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IncomeViz.ProfitCalculation.Migrations
+{
+    public partial class currencyconversion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                schema: "profitcalculation",
+                table: "ShortTermIncomes",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                schema: "profitcalculation",
+                table: "ShortTermExpenses",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                schema: "profitcalculation",
+                table: "LongTermIncomes",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                schema: "profitcalculation",
+                table: "LongTermExpenses",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Currency",
+                schema: "profitcalculation",
+                table: "ShortTermIncomes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Currency",
+                schema: "profitcalculation",
+                table: "ShortTermExpenses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Currency",
+                schema: "profitcalculation",
+                table: "LongTermIncomes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Currency",
+                schema: "profitcalculation",
+                table: "LongTermExpenses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
